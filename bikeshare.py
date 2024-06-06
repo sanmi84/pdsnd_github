dc09d706b62922474384d03bbf3aaf1c501f61e7
laboratorio <sanmi84@gmail.com>
2024-06-06 03:41:51 +0200

    Edit function def get_filters

--- bikeshare.py
+++ bikeshare.py
@@ -11,13 +11,13 @@ MONTHS = ['all', 'january', 'february', 'march', 'april', 'may', 'june']
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
-
     Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        (str) city - name of the city to analyze.
+        (str) month - name of the month to filter by, or "all" to apply no month filter.
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter.
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! ')
+    print('Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
         city = input("Choose a city 'chicago', 'new york city', 'washington': ").lower()
